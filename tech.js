4054eec5dc5a97d625e35d3e3c8e9bd9c60ad56b
tushar kumar <tbehera188@gmail.com>
2024-02-22 23:58:41 +0530

    add event on button

--- tech.js
+++ tech.js
@@ -1,2 +1,17 @@
-let x=5;
-console.log(x);
+let x=5,y=20;
+let sum=x+y;
+console.log(sum);
+
+let age=30;
+if(age>=18){
+    console.log("you can vote");
+}else{
+    console.log('you can not vote');
+}
+
+let a =document.querySelector(".divani");
+let v =document.getElementById("hit");
+v.addEventListener('click',function(){
+         a.innerHTML="thanku....."
+         
+});
