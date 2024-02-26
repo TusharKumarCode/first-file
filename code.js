6a93a00dcf309847e511f07cfa6e6277738f9d41
tushar kumar <tbehera188@gmail.com>
2024-02-26 16:30:25 +0530

    starting new code

--- code.js
+++ code.js
@@ -1,6 +1,12 @@
-console.log("hellooijfdfjoi");
 let x=5;
 let y=10;
 let z=x+y;
 console.log(z);
+
+let box=document.querySelector(".mod");
+let btn=document.querySelector("#loc");
+
+btn.addEventListener("click",function(){
+       box.style.backgroundColor="blue";
+});
 
