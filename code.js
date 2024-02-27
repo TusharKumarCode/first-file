abf04d2a56daabcb72f1851ccd22a229df1ce877
tushar kumar <tbehera188@gmail.com>
2024-02-27 16:53:02 +0530

    learning tailwind css

--- code.js
+++ code.js
@@ -3,10 +3,3 @@ let y=10;
 let z=x+y;
 console.log(z);
 
-let box=document.querySelector(".mod");
-let btn=document.querySelector("#loc");
-
-btn.addEventListener("click",function(){
-       box.style.backgroundColor="blue";
-});
-
