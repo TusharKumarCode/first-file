d424af917e5ce6b56389907dede5a48e5351d6f2
tushar kumar <tbehera188@gmail.com>
2024-02-28 16:22:43 +0530

    add event on button

--- code.js
+++ code.js
@@ -3,3 +3,13 @@ let y=10;
 let z=x+y;
 console.log(z);
 
+let btn=document.querySelector("#click");
+let change=document.querySelector("body");
+
+btn.addEventListener("click",function(){
+         change.innerHTML="<form><input placeholder=myname></form>";
+         change.style.background="linear-gradient(to top left,pink,blue,white,skyblue)";
+         change.style.height="645px";
+         
+});
+
