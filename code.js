49a323bfb7a41bef042dc2d6090a6d3c8cd4e7fe
tushar kumar <tbehera188@gmail.com>
2025-04-01 17:17:12 +0530

    adding basic animation

--- code.js
+++ code.js
@@ -1,42 +1,15 @@
-let x=5;
-let y=10;
-let z=x+y;
-console.log(z);
+let lgbtn=document.getElementById("logbtn");
+let lgform=document.getElementById("loginform");
 
-let btn=document.querySelector("#click");
-let change=document.querySelector("body");
-let crt=document.createElement("h1");
-let frm=document.createElement("input");
-let frm1=document.createElement("input");
-let log=document.createElement("button");
-btn.addEventListener("click",function(){
-         change.innerHTML="";
-         change.style.background="linear-gradient(to top left,pink,blue,white,skyblue)";
-         change.style.height="645px";
-         change.appendChild(crt);
-         crt.textContent="webname";
-        //  crt.setAttribute("class","text-green-600 text-9xl");
-         change.appendChild(frm);
-         frm.setAttribute("class","form");
-         frm.setAttribute("placeholder","email address");
-         crt.style.fontSize="6rem";
-         crt.style.color="white";
-         change.appendChild(frm1);
-         frm1.setAttribute("placeholder","password");
-         frm1.style.height="50px";
-         frm1.style.width="434px"
-         frm1.style.display="block";
-         frm1.style.marginTop="50px";
-         frm1.style.marginLeft="500px";
-         frm1.style.borderRadius="8px";
-         frm1.style.fontSize="20px";
-         frm1.style.textIndent="10%";
-         frm1.style.textTransform="capitalize";
-         frm1.style.fontWeight="bold";
-         change.appendChild(log);
-         log.textContent="sign in";
-         log.setAttribute("class","hit");
+lgbtn.addEventListener("click",function(){
+       lgform.setAttribute("class","h-screen w-full fixed top-0 left-0 z-10 flex justify-center items-center bg-[rgba(0,0,0,0.7)] block");
 });
+
+lgform.addEventListener("click",function(){
+      lgform.setAttribute("class","h-screen w-full fixed top-0 left-0 z-10 flex justify-center items-center bg-[rgba(0,0,0,0.7)] hidden");
+});
+
+
 
 let red=document.querySelector("#read");
 let cotent=document.querySelector("#inht");
@@ -57,11 +30,17 @@ store.addEventListener("click",function(){
 });
 
 let jam=document.querySelector("#fix");
+let place=document.getElementById("search");
+let fixicon=document.getElementById('icon');
 window.addEventListener("scroll",function(){
            if(window.scrollY>250){
-                 jam.setAttribute("class","fixed bg-green-300 top-0 w-full h-20 md:fixed");
+                 jam.setAttribute("class","fixed bg-green-300 top-0 w-full h-20 md:fixed flex justify-center items-center");
+                 place.setAttribute('class','w-[450px] h-14 rounded-xl focus:outline-none border-2 text-2xl indent-12');
+                 fixicon.setAttribute("class","fa-solid fa-magnifying-glass absolute left-[472px] text-xl top-[39%]");
            }else if(window.scrollY<250){
-               jam.setAttribute("class","bg-white w-68 md:w-3/4 h-16 md:ml-40 md:mt-24 rounded-xl mx-2 mt-40");
+                jam.setAttribute("class","w-68 md:w-[55%] md:mx-[23%] mx-2 h-20  md:mt-24 rounded-xl  mt-40 flex justify-center items-center relative");
+                place.setAttribute('class','w-full h-16 rounded-xl focus:outline-none border-2 text-2xl indent-20');
+                fixicon.setAttribute("class","fa-solid fa-magnifying-glass absolute left-12 text-2xl top-7");
            }
 });
 
